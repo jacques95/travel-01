b1eae2c5243f8305d7dd2ff139c08b2c50207caf
Jacques95 <jat95@qq.com>
2021-11-30 17:51:51 +0800

    perf: add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import { ProjectListScreen } from "./screens/project-list";
+// import { ProjectListScreen } from "./screens/project-list";
+import { LoginScreen } from "./login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      <LoginScreen/>
+      {/*<ProjectListScreen />*/}
     </div>
   );
 }
