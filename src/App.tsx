94b6a79773fe9751c33d12a90ecf90d48dcc75e1
Jacques95 <jat95@qq.com>
2021-11-30 12:03:44 +0800

    perf: add project_list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import { ProjectListScreen } from "./screens/project-list";
+
 function App() {
   return (
     <div className="App">
-      app ..
+      <ProjectListScreen />
     </div>
   );
 }
