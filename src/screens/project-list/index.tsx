1521bf592e7ac69551b3fb291c3b60b010a8af37
Jacques95 <jat95@qq.com>
2021-11-30 23:46:42 +0800

    feat: add token

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import * as qs from "qs";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { seriesParam, useDebounce, useMount } from "utils";
+import { useHttp } from "../../utils/http";
 
 const apiUrl = process.env.REACT_APP_URL;
 
@@ -16,21 +17,14 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
   const debouncedParam = useDebounce(param, 120);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(seriesParam(debouncedParam))}`).then(async response => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projets", { data: seriesParam(debouncedParam) }).then(setList);
   }, [param]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client('users').then(setUsers)
   });
 
   return <div>
