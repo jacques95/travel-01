cdde702ecdb03036089d9282cfc636cece489ffa
Jacques95 <jat95@qq.com>
2021-12-01 16:02:12 +0800

    build: initialize

---
+++ jira/src/utils/index.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const isFalsy = (value: unknown) => value === 0 ? false : value;
+export const clrParam = (param: object) => {
+  const result = { ...param };
+  Object.keys(result).forEach(key => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (cb: () => void) => {
+  useEffect(() => {
+    cb();
+  }, []);
+};
+
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
