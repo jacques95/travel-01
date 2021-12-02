406425f50d7a2fcda81e94f9739fd1ab0caa011b
Jacques95 <jat95@qq.com>
2021-12-02 14:41:53 +0800

    feat: add project_list

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ProjectListScreen } from "./screens/project-list";
 
 function App() {
   return (
     <div className="App">
-      app ...
+      <ProjectListScreen />
     </div>
   );
 }
