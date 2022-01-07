76e5165095d6125392c5d20fbfad73c84e4267a1
Jacques95 <jat95@qq.com>
2022-01-07 17:44:32 +0800

    git feat: context

--- jira/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { loadDevTools } from "jira-dev-tool";
-
-loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-});
