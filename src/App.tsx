4ca415a181800a0d7f8f0dd1c5d303da2625c6f4
Jacques95 <jat95@qq.com>
2022-02-08 10:01:18 +0800

    ci: init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+    return (
+        <div className={styles.app}>
+            app ...
+        </div>
+    );
+}
+
+export default App;
