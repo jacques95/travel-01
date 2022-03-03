5a1c620568c2bf20158c5c29ae4cd27727ed47af
Jacques <jat95@qq.com>
2022-03-03 21:45:06 +0800

    ci: initialize

---
+++ travel/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+export const App: React.FC = () => {
+    return (
+        <div className={styles.App}>
+            app...
+        </div>
+    );
+}
