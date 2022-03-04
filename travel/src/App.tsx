54eda2b4cdc08bab6d32b5a053a93f97d6ad0738
Jacques <jat95@qq.com>
2022-03-04 15:44:48 +0800

    refactor: header

--- travel/src/App.tsx
+++ travel/src/App.tsx
@@ -1,58 +1,12 @@
 import React from 'react';
-import logo from './assets/logo.gif';
 import styles from './App.module.css';
-import {Layout, Typography, Input, Menu, Button, Dropdown} from 'antd'
-import {GlobalOutlined} from '@ant-design/icons'
 
+import {Footer, Header} from './components'
 export default function App() {
     return (
         <div className={styles.App}>
-
-            <div className={styles['app-header']}>
-
-                <div className={styles['top-header']}>
-                    <div className={styles.inner}>
-                        <Typography.Text >让步伐随心而动</Typography.Text>
-                        <Dropdown.Button
-                            style={{marginLeft: 15}}
-                            overlay={
-                                <Menu>
-                                    <Menu.Item>中文</Menu.Item>
-                                    <Menu.Item>English</Menu.Item>
-                                </Menu>
-                            }
-                            icon={<GlobalOutlined/>}
-                        >语言</Dropdown.Button>
-                        <Button.Group className={styles['button-group']}>
-                            <Button>注册</Button>
-                            <Button>登陆</Button>
-                        </Button.Group>
-                    </div>
-                </div>
-
-                <Layout.Header className={styles['main-header']}>
-                    <img src={logo} alt="logo" className={styles.logo}/>
-                    <Typography.Title className={styles.title}
-                                      level={3}>旅游网</Typography.Title>
-                    <Input.Search
-                        className={styles['search-input']}
-                        placeholder={'请输入目的地、主题、或关键字'}/>
-                </Layout.Header>
-
-                <Menu mode={'horizontal'} className={styles['main-menu']}>
-                    <Menu.Item key={1}>首页</Menu.Item>
-                    <Menu.Item key={2}>周未游</Menu.Item>
-                    <Menu.Item key={3}>跟团游</Menu.Item>
-                    <Menu.Item key={4}>自由行</Menu.Item>
-                    <Menu.Item key={5}>私家团</Menu.Item>
-                    <Menu.Item key={6}>定制游</Menu.Item>
-                </Menu>
-            </div>
-
-            <Layout.Footer>
-                <Typography.Title level={3} style={{textAlign: 'center'}}>版本所有
-                    @ 旅游网</Typography.Title>
-            </Layout.Footer>
+            <Header/>
+            <Footer/>
         </div>
     );
 }
