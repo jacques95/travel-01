0c45b0af9f389c117370c998db3f3131a0b6d901
Jacques <jat95@qq.com>
2022-03-04 08:40:33 +0800

    ci: initialize

--- travel/src/App.tsx
+++ travel/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
-export const App: React.FC = () => {
-    return (
-        <div className={styles.App}>
-            app...
-        </div>
-    );
+export default function App() {
+  return (
+    <div className={styles.App}>
+      app...
+    </div>
+  );
 }
--- travel/src/index.tsx
+++ travel/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css'
-import {App} from './App';
+import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
