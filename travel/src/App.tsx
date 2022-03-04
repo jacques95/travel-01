650dee5be60e84df2e38bb830eb8b72c542a349a
Jacques <jat95@qq.com>
2022-03-04 21:53:52 +0800

    ci: initialize

--- travel/src/App.tsx
+++ travel/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
-    return (
-        <div className={styles.App}>
-            app ...
-        </div>
-    );
+  return <div className={styles.App}>
+      App ...
+    </div>
 }
 
 export default App;
